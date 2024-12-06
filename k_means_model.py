ae414780e07e6401828baf0a02675b72d87978f6
NzubeHeritage <nzubegodswil2k@gmail.com>
2024-12-06 13:56:06 -0500

    Initial commit

---
+++ k_means_model.py
@@ -0,0 +1,54 @@
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import joblib
+
+#load and process data
+df = pd.read_csv("data/data_clustering.csv")
+
+print(df.head(10))
+
+# Assign values X
+X = df.iloc[:, :].values
+print(X)
+print(X.shape)
+
+#using the elbow method to find the optimal number of clusters
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init="k-means++", random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('wcss')
+plt.show()
+
+#Training the k-means model on the dataset
+kmeans = KMeans(n_clusters=5, init="k-means++", random_state=42)
+y_pred = kmeans.fit_predict(X)
+
+# Save the trained model
+joblib.dump(kmeans, "model/model.pkl")
+
+# Predictions
+# 0: "Prudent Spender",
+# 1: "Generous Spender",
+# 2: "Extravagant Spender",
+# 3: "Wise Spender",
+# 4: "Loose Spender"
+plt.scatter(X[y_pred == 0, 0], X[y_pred == 0, 1], s=100, c='red', label='Prudent Spenders')
+plt.scatter(X[y_pred == 1, 0], X[y_pred == 1, 1], s=100, c='blue', label='Generous Spenders')
+plt.scatter(X[y_pred == 2, 0], X[y_pred == 2, 1], s=100, c='green', label='Extravagant Spenders')
+plt.scatter(X[y_pred == 3, 0], X[y_pred == 3, 1], s=100, c='cyan', label='Wise Spenders')
+plt.scatter(X[y_pred == 4, 0], X[y_pred == 4, 1], s=100, c='magenta', label='Loose Spenders')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
